bbc0de730547072ca605a266fcd063c523fcbf00
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2025-08-05 11:41:32 -0300

    feat: Implement custom 404 page with style and informative message

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+import styles from "./not-found.module.css";
+
 export default function NotFound() {
-    return (
-        <h1>Olaaaaaa</h1>
-    )
+  return (
+    <div className={styles.notFoundContainer}>
+      <hgroup className={styles.hgroup}>
+        <h1 className={styles.title}>404</h1>
+        <h2 className={styles.subtitle}>Oops... page not found</h2>
+        <h3 className={styles.message}>Sorry, this page doesn't exist. We are working now to correct errors and bugs!</h3>
+      </hgroup>
+
+      <p className={styles.info}>
+        To return to a safe page, press the button below or click the back arrow at the top of the page.
+      </p>
+
+      <Link href="/" passHref legacyBehavior>
+        <button className={styles.button}>Go to Home</button>
+      </Link>
+    </div>
+  );
 }
