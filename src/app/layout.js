ab3dbd093453ec654a0a0b23ef73a505bfa4408d
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2025-08-05 10:35:16 -0300

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,27 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "MARTHI - TEC",
+  description: "Venda de eletrônicos novos e que ainda não foram lançados!",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
